95beb9ed6c1d1df00dde346e7c9853892f3e8dd5
CandyWall <851702778@qq.com>
2024-05-01 01:27:33 +0800

    优化项目配置

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,20 @@
-import * as vscode from 'vscode';
+import * as vscode from 'vscode'
 import * as fs from 'fs'
 
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(
 		vscode.commands.registerCommand('vue.open', () => {
-			CatCodingPanel.createOrShow(context.extensionUri);
+			CatCodingPanel.createOrShow(context.extensionUri)
 		})
-	);
+	)
 
 	context.subscriptions.push(
-		vscode.commands.registerCommand('catCoding.doRefactor', () => {
+		vscode.commands.registerCommand('vue.doRefactor', () => {
 			if (CatCodingPanel.currentPanel) {
-				CatCodingPanel.currentPanel.doRefactor();
+				CatCodingPanel.currentPanel.doRefactor()
 			}
 		})
-	);
+	)
 }
 
 function getWebviewOptions(extensionUri: vscode.Uri): vscode.WebviewOptions {
@@ -22,7 +22,7 @@ function getWebviewOptions(extensionUri: vscode.Uri): vscode.WebviewOptions {
 		// Enable javascript in the webview
 		enableScripts: true,
 		localResourceRoots: [vscode.Uri.joinPath(extensionUri, 'views')]
-	};
+	}
 }
 
 /**
@@ -32,42 +32,42 @@ class CatCodingPanel {
 	/**
 	 * Track the currently panel. Only allow a single panel to exist at a time.
 	 */
-	public static currentPanel: CatCodingPanel | undefined;
+	public static currentPanel: CatCodingPanel | undefined
 
-	public static readonly viewType = 'catCoding';
+	public static readonly viewType = 'catCoding'
 
-	private readonly _panel: vscode.WebviewPanel;
-	private readonly _extensionUri: vscode.Uri;
-	private _disposables: vscode.Disposable[] = [];
+	private readonly _panel: vscode.WebviewPanel
+	private readonly _extensionUri: vscode.Uri
+	private _disposables: vscode.Disposable[] = []
 
 	public static createOrShow(extensionUri: vscode.Uri) {
 		const column = vscode.window.activeTextEditor
 			? vscode.window.activeTextEditor.viewColumn
-			: undefined;
+			: undefined
 
 		// If we already have a panel, show it.
 		if (CatCodingPanel.currentPanel) {
-			CatCodingPanel.currentPanel._panel.reveal(column);
-			return;
+			CatCodingPanel.currentPanel._panel.reveal(column)
+			return
 		}
 
 		// Otherwise, create a new panel.
 		const panel = vscode.window.createWebviewPanel(
 			CatCodingPanel.viewType,
-			'Cat Coding',
+			'Webview Vue Sample',
 			column || vscode.ViewColumn.One,
 			getWebviewOptions(extensionUri),
-		);
+		)
 
-		CatCodingPanel.currentPanel = new CatCodingPanel(panel, extensionUri);
+		CatCodingPanel.currentPanel = new CatCodingPanel(panel, extensionUri)
 	}
 
 	private constructor(panel: vscode.WebviewPanel, extensionUri: vscode.Uri) {
-		this._panel = panel;
-		this._extensionUri = extensionUri;
+		this._panel = panel
+		this._extensionUri = extensionUri
 		// Listen for when the panel is disposed
 		// This happens when the user closes the panel or when the panel is closed programmatically
-		this._panel.onDidDispose(() => this.dispose(), null, this._disposables);
+		this._panel.onDidDispose(() => this.dispose(), null, this._disposables)
 
 		this._panel.webview.html = this._getHtmlForWebview(panel.webview).replaceAll("{{contextPath}}", panel.webview.asWebviewUri(vscode.Uri.joinPath(extensionUri, "views", "dist")).toString())
 
@@ -76,43 +76,43 @@ class CatCodingPanel {
 			message => {
 				switch (message.command) {
 					case 'alert':
-						vscode.window.showInformationMessage(message.text);
-						return;
+						vscode.window.showInformationMessage(message.text)
+						return
 				}
 			},
 			null,
 			this._disposables
-		);
+		)
 	}
 
 	public doRefactor() {
 		// Send a message to the webview webview.
 		// You can send any JSON serializable data.
-		this._panel.webview.postMessage({ command: 'refactor' });
+		this._panel.webview.postMessage({ command: 'refactor' })
 	}
 
 	public dispose() {
-		CatCodingPanel.currentPanel = undefined;
+		CatCodingPanel.currentPanel = undefined
 
 		// Clean up our resources
-		this._panel.dispose();
+		this._panel.dispose()
 
 		while (this._disposables.length) {
-			const x = this._disposables.pop();
+			const x = this._disposables.pop()
 			if (x) {
-				x.dispose();
+				x.dispose()
 			}
 		}
 	}
 
 	private _getHtmlForWebview(webview: vscode.Webview) {
 		// Local path to index.html in the webview
-		const indexPath = vscode.Uri.joinPath(this._extensionUri, './views/dist/', 'index.html');
+		const indexPath = vscode.Uri.joinPath(this._extensionUri, './views/dist/', 'index.html')
 		// // Uri to load styles into webview
-		// const stylesResetUri = webview.asWebviewUri(indexPath);
+		// const stylesResetUri = webview.asWebviewUri(indexPath)
 		console.log(webview.asWebviewUri(indexPath))
 		// Use a nonce to only allow specific scripts to be run
 		
-		return fs.readFileSync(indexPath.fsPath).toString();
+		return fs.readFileSync(indexPath.fsPath, 'utf-8')
 	}
 }
